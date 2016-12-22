395845a6c36b2e9793af790159f8011319827dd4
Ben Ebdon <ben.david.ebdon@gmail.com>
2016-12-22 09:40:30 +0000

    Project Creation and Json Testing
    - Created windows form project in C#
    - Tested URL Connections to parse JSON
    - Tested JSON to Objects

---
+++ windowsDesktopClient/windowsDesktopClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Text;
+
+namespace windowsDesktopClient
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Returns JSON string
+            string GET(string url)
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                try
+                {
+                    WebResponse response = request.GetResponse();
+                    using (Stream responseStream = response.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                        return reader.ReadToEnd();
+                    }
+                }
+                catch (WebException ex)
+                {
+                    WebResponse errorResponse = ex.Response;
+                    using (Stream responseStream = errorResponse.GetResponseStream())
+                    {
+                        StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                        String errorText = reader.ReadToEnd();
+                        // log errorText
+                    }
+                    throw;
+                }
+            }
+
+            var jsonRequest = GET("http://api.uoltt.org/api/v4/ships/1");
+
+            var jsonObject = JsonConvert.DeserializeObject(jsonRequest);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Home());
+        }
+    }
+}
