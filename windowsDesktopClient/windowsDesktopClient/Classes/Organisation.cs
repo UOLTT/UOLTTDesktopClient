e9329e9b719b3b03afbe5284b6b9eeb93df11f0c
Ben Ebdon <ben.david.ebdon@gmail.com>
2016-12-22 18:58:51 +0000

    Added Org, User and moved Ship Classes Added New Control for Testing

---
+++ windowsDesktopClient/windowsDesktopClient/Classes/Organisation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windowsDesktopClient.Classes
+{
+    public class Organisation
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Domain { get; set; }
+        public int AdminUserId { get; set; }
+        public int StatusId { get; set; }
+        public string Manifesto { get; set; }
+        public string CreatedAt { get; set; }
+        public string UpdatedAt { get; set; }
+        public int UserCount { get; set; }
+    }
+}
---
+++ windowsDesktopClient/windowsDesktopClient/Classes/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace windowsDesktopClient.Classes
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? OrganisationId { get; set; }
+        public int? FleedId { get; set; }
+        public int? SquadId { get; set; }
+        public int? StationId { get; set; }
+        public int? ShipUserId { get; set; }
+    }
+}
